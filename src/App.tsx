40f49844b96682d29f1244585a6aa3248fa02cb5
cjsupan <supancj18@gmail.com>
2026-06-30 00:45:19 +0800

    fix Technology import

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./styles/App.css";
 import ThemeToggle from "./components/ThemeToggle";
 import Home from "./sections/Home";
 import About from "./sections/About";
-import Tecnologies from "./sections/Technologies";
+import Technologies from "./sections/Technologies";
 import Projects from "./sections/Projects";
 import Contact from "./sections/Contact";
 
@@ -13,7 +13,7 @@ function App() {
       <main className="w-full h-full dark:bg-background-dark bg-background-light dark:text-primary-dark transition duration-500 ease-in-out flex flex-col justify-center items-center @max-md:pt-20 ">
         <Home />
         <About />
-        <Tecnologies />
+        <Technologies />
         <Projects />
         <Contact />
       </main>
